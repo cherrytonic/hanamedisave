a4e359094173211a3698d47ad432b77e98a1a5a0
원채령 <fairy3530@naver.com>
2024-10-10 23:37:19 +0900

    feat:cors

--- backend/src/main/java/com/medisave/backend/config/WebMvcConfig.java
+++ backend/src/main/java/com/medisave/backend/config/WebMvcConfig.java
@@ -1,9 +1,7 @@
 package com.medisave.backend.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,27 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // React 앱의 주소
-        config.addAllowedOrigin("http://211.188.50.141"); // React 앱의 주소
-        config.addAllowedOrigin("https://211.188.50.141"); // React 앱의 주소
-        config.addAllowedOrigin("https://www.hanamedisave.site");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
-    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://211.188.50.141", "https://211.188.50.141", "https://www.hanamedisave.site")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
