ccc493d34242dc8be2a68a39b0a0716a15ffb48b
원채령 <fairy3530@naver.com>
2024-10-10 13:54:31 +0900

    feat: https

--- react/src/api/medisave.js
+++ react/src/api/medisave.js
@@ -4,8 +4,8 @@ axios.defaults.withCredentials = false;
 
 export default axios.create({
   // baseURL: 'http://211.188.50.141:8080',
-  baseURL: 'http://backend:8080',
-  // baseURL: 'http://localhost:8080',
+  // baseURL: 'http://backend:8080',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Accept': 'application/json',
     // 'Access-Control-Allow-Origin': '*',
