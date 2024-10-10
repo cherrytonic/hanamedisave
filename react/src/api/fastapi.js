9e8f688a13d4344918e7f42c24bc8bf5375827dc
원채령 <fairy3530@naver.com>
2024-10-10 13:35:47 +0900

    feat: https

--- react/src/api/fastapi.js
+++ react/src/api/fastapi.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:8000/api/fast',
-  baseURL: 'http://211.188.50.141:8000/api/fast',
+  // baseURL: 'http://211.188.50.141:8000/api/fast',
+  baseURL: 'http://fastapi:8000/api/fast',
   // baseURL: '/api/fast',
   headers: {
     'Content-Type': 'multipart/form-data',
--- react/src/api/medisave.js
+++ react/src/api/medisave.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 axios.defaults.withCredentials = false;
 
 export default axios.create({
-  baseURL: 'http://211.188.50.141:8080',
+  // baseURL: 'http://211.188.50.141:8080',
+  baseURL: 'http://backend:8080',
   // baseURL: 'http://localhost:8080',
   headers: {
     'Accept': 'application/json',
